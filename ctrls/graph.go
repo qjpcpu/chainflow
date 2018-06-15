5393faae7843ec609f52294b22b136c8aa036559
JasonQu <qjpcpu@gmail.com>
2018-06-15 13:11:02 +0800

    update

--- ctrls/graph.go
+++ ctrls/graph.go
@@ -2,7 +2,6 @@ package ctrls
 
 import (
 	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/qjpcpu/chainflow/db"
 	"github.com/qjpcpu/chainflow/network"
@@ -60,8 +59,8 @@ type NetworkGraph struct {
 func QueryNetwork(c *gin.Context) {
 	var err error
 	var ng NetworkGraph
+	contract := strings.ToLower(c.Query("contract"))
 	for loop := true; loop; loop = false {
-		contract := c.Query("contract")
 		if contract == "" {
 			err = errors.New("no contract")
 			break
@@ -95,13 +94,7 @@ func QueryNetwork(c *gin.Context) {
 		if len(paths) == 0 {
 			break
 		}
-		if len(paths) > 1000 {
-			paths = paths[:1000]
-		}
-		var txs []db.TokenTransfer
-		o := db.GetOrm()
-		tmStart = time.Now()
-		var fromto []interface{}
+
 		for _, p := range paths {
 			nodes[p.From] = NetworkNode{
 				Id:      p.From,
@@ -117,55 +110,26 @@ func QueryNetwork(c *gin.Context) {
 			}
 			switch direction {
 			case network.In:
-				fromto = append(fromto, db.FromTo(p.To, p.From))
+				ng.Edges = append(ng.Edges, NetworkEdge{
+					Source: p.To,
+					Target: p.From,
+				})
 			default:
-				fromto = append(fromto, db.FromTo(p.From, p.To))
+				ng.Edges = append(ng.Edges, NetworkEdge{
+					Source: p.From,
+					Target: p.To,
+				})
 			}
 		}
-		offset := 0
-		step := 100
-	LOOP:
-		for {
-			end := offset + step
-			if end > len(fromto) {
-				end = len(fromto)
-			}
-			if offset >= end {
-				break LOOP
-			}
-			sql := fmt.Sprintf("select * from token_transfer where id in (select max(id) from token_transfer where contract=? and from_to in (%s) group by from_to)", strings.TrimSuffix(strings.Repeat("?,", end-offset), ","))
-			args := append([]interface{}{strings.ToLower(contract)}, fromto[offset:end]...)
-			var ntxs []db.TokenTransfer
-			if _, err = o.Raw(sql, args...).QueryRows(&ntxs); err != nil {
-				break LOOP
-			}
-			txs = append(txs, ntxs...)
-			if end >= len(fromto) || offset+step >= len(fromto) {
-				break LOOP
-			}
-			offset += step
-		}
-		if err != nil {
-			break
-		}
-		tmEnd = time.Now()
-		log.Infof("query db cost:%v", tmEnd.Sub(tmStart))
+
 		for _, node := range nodes {
 			ng.Nodes = append(ng.Nodes, node)
-		}
-		for _, tx := range txs {
-			ng.Edges = append(ng.Edges, NetworkEdge{
-				Source: tx.From,
-				Target: tx.To,
-				Tx:     tx.Tx,
-				Amount: tx.Value,
-			})
 		}
 	}
 	if err != nil {
 		log.Errorf("get network fail:%v", err)
 		c.JSON(http.StatusOK, gin.H{"code": 1, "msg": err.Error()})
 	} else {
-		c.JSON(http.StatusOK, gin.H{"code": 0, "network": ng})
+		c.JSON(http.StatusOK, gin.H{"code": 0, "network": ng, "contract": contract})
 	}
 }
