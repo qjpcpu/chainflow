1b6e3f3ed18a0f80db114383f2336a267197f2bb
JasonQu <qjpcpu@gmail.com>
2018-06-15 13:14:51 +0800

    x

--- ctrls/graph.go
+++ ctrls/graph.go
@@ -91,7 +91,7 @@ func QueryNetwork(c *gin.Context) {
 		nodes := make(map[string]NetworkNode)
 		var tmStart, tmEnd time.Time
 		tmStart = time.Now()
-		paths := ns.NetworkOf(strings.ToLower(address), network.Pred_Transfer, 1, direction)
+		paths := ns.NetworkOf(strings.ToLower(address), network.Pred_Transfer, 2, direction)
 		tmEnd = time.Now()
 		log.Infof("query cayley cost:%v,get %v path", tmEnd.Sub(tmStart), len(paths))
 		if len(paths) == 0 {
